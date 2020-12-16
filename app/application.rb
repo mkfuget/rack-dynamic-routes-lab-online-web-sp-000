ef5a71b8d439c8c9940bb5b3b2434f89f595c722
Michael Fuget <michaelfuget@gmail.com>
2020-12-16 06:26:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,8 @@ require_relative './item'
 class Application
   def call(env)
     resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
     if req.path.match(/items/)
       current_item_name = req.path.match(/items/)[0][6..-1]
       current_item = Item.all.find?{|item| item.name == current_item_name}
