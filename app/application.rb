d1d697a0f726bd9ad2b092949eff4113050cb89d
Michael Fuget <michaelfuget@gmail.com>
2020-12-16 06:22:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
     resp = Rack::Response.new
     if req.path.match(/items/)
       item = req.path.match(/items/)[0][6..-1]
-      
+      if(Item.all.include?(item))
 
       
     resp.finish
