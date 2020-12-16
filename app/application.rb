6653e4c3ede242c0f892e4e9755e8b09ad5f8873
Michael Fuget <michaelfuget@gmail.com>
2020-12-16 06:24:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,9 @@ class Application
     resp = Rack::Response.new
     if req.path.match(/items/)
       current_item_name = req.path.match(/items/)[0][6..-1]
+      current_item = 
       if(Item.all.find?(|item| item.name == current_item_name))
+        return 
 
       
     resp.finish
