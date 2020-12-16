df3b89a7121b9bece96962263d9a75585934e6ac
Michael Fuget <michaelfuget@gmail.com>
2020-12-16 06:21:47 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,10 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    current_hour = Time.now.hour
     if req.path.match(/items/)
+      item = req.path.match(/items/)[0][6..-1]
       
+
       
     resp.finish
   end
