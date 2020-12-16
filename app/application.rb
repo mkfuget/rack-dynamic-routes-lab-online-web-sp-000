a8082576e76adff005af21f454ad6060871eb97a
Michael Fuget <michaelfuget@gmail.com>
2020-12-16 06:17:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,12 +2,8 @@ class Application
   def call(env)
     resp = Rack::Response.new
     current_hour = Time.now.hour
-    if(current_hour<12)
-      resp.write "Good Morning!"
-    else 
-      resp.write "Good Afternoon!"
-    end
-
+    if req.path.match(/items/)
+      
       
     resp.finish
   end
